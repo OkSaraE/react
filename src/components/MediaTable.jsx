76d8ca348ee5aefa96223b1b73c12376f6de4e86
sarao <saraok@metropolia.fi>
2023-03-17 11:57:01 +0200

    custom-hooks

--- src/components/MediaTable.jsx
+++ src/components/MediaTable.jsx
@@ -1,31 +1,8 @@
-import {useEffect, useState} from 'react';
-import {baseUrl} from '../utils/variables';
+import {useMedia} from '../../hooks/apiHooks';
 import MediaRow from './Mediarow';
 
 const MediaTable = () => {
-  const [mediaArray, setMeadiaArray] = useState([]);
-  const getMedia = async () => {
-    const response = await fetch(baseUrl + 'media');
-    const files = await response.json();
-    const filesWithThumbnail = await Promise.all(
-      files.map(async (file) => {
-        const response = await fetch(baseUrl + 'media/' + file.file_id);
-        return await response.json();
-      })
-    );
-    setMeadiaArray(filesWithThumbnail);
-  };
-
-  useEffect(() => {
-    try {
-      getMedia();
-    } catch (error) {
-      console.log(error.message);
-    }
-  }, []);
-
-  console.log(mediaArray);
-
+  const {mediaArray} = useMedia();
   return (
     <table>
       <tbody>
